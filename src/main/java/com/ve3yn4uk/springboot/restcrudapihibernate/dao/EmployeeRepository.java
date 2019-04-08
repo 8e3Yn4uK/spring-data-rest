cb802efdac6f0c93f4aabf3df3cf73259f269888
8e3Yn4uK <sashko.kondratenko@gmail.com>
2019-04-08 17:45:47 +0200

    ref

--- src/main/java/com/ve3yn4uk/springboot/restcrudapihibernate/dao/EmployeeRepository.java
+++ src/main/java/com/ve3yn4uk/springboot/restcrudapihibernate/dao/EmployeeRepository.java
@@ -2,10 +2,12 @@ package com.ve3yn4uk.springboot.restcrudapihibernate.dao;
 
 import com.ve3yn4uk.springboot.restcrudapihibernate.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * Created by 8e3Yn4uK on 08.04.2019
  */
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
